c0f7e8bbad76c921433f24cff251f15383f5e938
gauravk-io <gauravkr7265@gmail.com>
2025-06-11 14:52:34 +0530

    send and verify email

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { generateKey } from "crypto";
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+import { sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js";
 
 export const signup = async (req, res) => {
     const { email, password, name } = req.body;
@@ -34,6 +35,8 @@ export const signup = async (req, res) => {
 
         generateTokenAndSetCookie(res, user._id);
 
+        await sendVerificationEmail(user.email, verificationToken);
+
         res.status(201).json({
             success: true,
             message: "User created successfully",
@@ -46,6 +49,39 @@ export const signup = async (req, res) => {
 
     } catch (error) {
         res.status(400).json({ success: false, message: error.message });
+    }
+};
+
+export const verifyEmail = async (req, res) => {
+    const { code } = req.body;
+    try {
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationTokenExpiresAt: { $gt: Date.now() },
+        });
+
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Invalid or expired verification code" });
+        }
+
+        user.isVerified = true;
+        user.verificationToken = undefined;
+        user.verificationTokenExpiresAt = undefined;
+        await user.save();
+
+        await sendWelcomeEmail(user.email, user.name);
+
+        res.status(200).json({
+            success: true,
+            message: "Email verified successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            },
+        });
+    } catch (error) {
+        console.error("Error in verifyEmail ", error);
+        res.status(500).json({ success: false, message: "Server error" });
     }
 };
 
