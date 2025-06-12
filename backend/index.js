56abcfeb56cc11fc4c51ff3455358bed9d7deccc
gauravk-io <gauravkr7265@gmail.com>
2025-06-12 14:48:19 +0530

    backend completed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 // import { connect } from 'mongoose';
+import cookieParser from 'cookie-parser';
+
 import { connectDB } from './db/connectDB.js';
 
 import authRoutes from "./routes/auth.route.js"
@@ -11,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to parse incoming requests: req.body
+app.use(cookieParser()); // allow us to parse incoming cookies
 
 app.use("/app/auth", authRoutes);
 
